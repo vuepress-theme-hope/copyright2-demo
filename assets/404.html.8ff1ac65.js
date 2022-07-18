2721bcbd1804c4e6422e007e1654ecba102d78e4
Mister-Hope <mister-hope@outlook.com>
2022-07-18 14:55:19 +0000

    Deploying to gh-pages from @ vuepress-theme-hope/vuepress-theme-hope@97e916598a0c6d30ab01893146fb54190c6f58d7 🚀

---
+++ assets/404.html.8ff1ac65.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-3706649a","path":"/404.html","title":"","lang":"en-US","frontmatter":{"layout":"404"},"excerpt":"","headers":[],"git":{},"filePathRelative":null,"copyright":"Copyright by Mr.Hope\\nLicense under MIT\\nhttps://vuepress-theme-hope.github.io/copyright2-demo/404.html"}');export{e as data};
---
+++ assets/index.html.291cbbbf.js
@@ -0,0 +1,12 @@
+import{_ as n,o as s,c as a,a as e}from"./app.36d206e4.js";const t={},i=e(`<h2 id="demo" tabindex="-1"><a class="header-anchor" href="#demo" aria-hidden="true">#</a> Demo</h2><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code><span class="token comment">// .vuepress/config.js</span>
+module<span class="token punctuation">.</span>exports <span class="token operator">=</span> <span class="token punctuation">{</span>
+  <span class="token literal-property property">plugins</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">[</span>
+      <span class="token string">&quot;copyright2&quot;</span><span class="token punctuation">,</span>
+      <span class="token punctuation">{</span>
+        <span class="token comment">// options</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[i];function p(o,l){return s(),a("div",null,c)}var r=n(t,[["render",p],["__file","index.html.vue"]]);export{r as default};
